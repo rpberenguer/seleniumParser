8f0e588cb5ae34ca9257da715ff7eabe25e4596b
rperez <raul.perez@innofis.com>
2018-08-03 14:41:31 +0200

    spring boot

--- src/main/java/es/fantasymanager/AlertManagerRootConfig.java
+++ src/main/java/es/fantasymanager/AlertManagerRootConfig.java
@@ -3,14 +3,9 @@ package es.fantasymanager;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.innofis.omnichannel.ecorp.alertmanager.configuration.hystrix.shutdown.HystrixShutdownHandler;
-import com.innofis.omnichannel.ecorp.alertmanager.configuration.ribbon.RibbonConfiguration;
 
 /**
  * Basic root configuration that scans the Configuration classes defining
@@ -18,10 +13,6 @@ import com.innofis.omnichannel.ecorp.alertmanager.configuration.ribbon.RibbonCon
  */
 
 @SpringBootApplication(scanBasePackages = "com.innofis.omnichannel")
-@EnableDiscoveryClient
-@EnableCircuitBreaker
-@EnableJpaAuditing
-@RibbonClient(name = "${spring.application.name}", configuration = RibbonConfiguration.class)
 public class AlertManagerRootConfig extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
