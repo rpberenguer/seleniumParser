49089baeeb8f6a80552786826f7c081ca4f68f66
rperez <raul.perez@innofis.com>
2018-09-04 17:37:53 +0200

    fixes

--- src/main/java/es/fantasymanager/utils/SeleniumGridDockerHub.java
+++ src/main/java/es/fantasymanager/utils/SeleniumGridDockerHub.java
@@ -6,40 +6,38 @@ import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
- 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SeleniumGridDockerHub {
- 
-    //Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
-    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
- 
-   
-    public void setupDriver (String browser) throws MalformedURLException {
-        //Set DesiredCapabilities
-        DesiredCapabilities capabilities = new DesiredCapabilities();
- 
-        //Set BrowserName
-        capabilities.setCapability("browserName", browser);
-        
-        //Set Browser to ThreadLocalMap
-        driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
-    }
- 
- 
-    public void quitDriver() throws Exception {
-        getDriver().quit();
-    }
- 
-    public void removeDriver () {
-        //Remove the ThreadLocalMap element
-        driver.remove();
-    }
-    
-    public WebDriver getDriver() {
-    	//Get driver from ThreadLocalMap
-    	return driver.get();
-    }
+
+	//Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
+	protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+
+
+	public void setupDriver (String browser) throws MalformedURLException {
+		//Set DesiredCapabilities
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		//Set BrowserName
+		capabilities.setCapability("browserName", browser);
+
+		//Set Browser to ThreadLocalMap
+		driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
+	}
+
+
+	public void quitDriver() throws Exception {
+		getDriver().quit();
+	}
+
+	public void removeDriver () {
+		//Remove the ThreadLocalMap element
+		driver.remove();
+	}
+
+	public WebDriver getDriver() {
+		//Get driver from ThreadLocalMap
+		return driver.get();
+	}
 }
