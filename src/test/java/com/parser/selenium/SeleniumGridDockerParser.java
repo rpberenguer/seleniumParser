228783028679db883a9b30bcb533d563529f11bd
raul <rpberenguer@gmail.com>
2018-09-27 00:42:10 +0200

    parser fixes

--- src/test/java/com/parser/selenium/SeleniumGridDockerParser.java
+++ src/test/java/com/parser/selenium/SeleniumGridDockerParser.java
@@ -1,18 +1,13 @@
 package com.parser.selenium;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import com.google.common.base.Function;
 
 import es.fantasymanager.utils.Constants;
 
@@ -27,7 +22,7 @@ public class SeleniumGridDockerParser extends TestBase implements Constants {
 		// Get driver
 		WebDriver driver = getDriver();
 
-		driver.get(URL_ESPN);
+		driver.get(URL_ADD_PLAYERS);
 		driver.switchTo().defaultContent(); // you are now outside both frames
 		driver.switchTo().frame(LOGIN_IFRAME);
 		driver.getPageSource();
@@ -40,7 +35,7 @@ public class SeleniumGridDockerParser extends TestBase implements Constants {
 		email.sendKeys("rpberenguer@gmail.com");
 
 		final WebElement password = driver.findElement(BY_PASSWORD_INPUT);
-		password.sendKeys("8ad3aah4");
+		password.sendKeys("*****");
 
 		final WebElement signupButton = driver
 				.findElement(BY_SUBMIT_LOGIN_BUTTON);
@@ -48,7 +43,7 @@ public class SeleniumGridDockerParser extends TestBase implements Constants {
 
 		// Wait WebDriver
 		driver.switchTo().defaultContent();
-//		WebDriverWait wait = new WebDriverWait(driver, 90);
+		WebDriverWait wait = new WebDriverWait(driver, 90);
 		
 //		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)       
 //				.withTimeout(20, TimeUnit.SECONDS)    
@@ -63,27 +58,38 @@ public class SeleniumGridDockerParser extends TestBase implements Constants {
 //		
 //		aboutMe.click();
 
+		System.out.println("111111111111");
 		// Wait until Tab is clickable
-//		WebElement playerTab = wait.until(ExpectedConditions.elementToBeClickable(BY_PLAYERS_TAB));
-//		WebElement playerTab = driver.findElement(BY_PLAYERS_TAB);
-//		playerTab.click();
-		
-		driver.get(URL_PLAYERS);
-
-		// Find (+) Add Player Linkg & click()
-		WebElement addPlayerLink = driver.findElement(By.cssSelector("#plyr" + playerToAdd + " td:nth-of-type(4) > a"));
+		WebElement addPlayerLink = wait.until(ExpectedConditions.elementToBeClickable(BY_ADD_PLAYER_LINK));
 		addPlayerLink.click();
-
+		
+		System.out.println("2222222222222");
+		
 		// Find checkBox to Remove Player
-		WebElement removePlayerCheck = driver.findElement(By.cssSelector("#plyr" + playerToRemove + " td:nth-of-type(1) > input"));
-		removePlayerCheck.click();
+		WebElement removePlayerLink = wait.until(ExpectedConditions.elementToBeClickable(BY_REMOVE_PLAYER_LINK));
+		
+		// js executor
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", removePlayerLink);
+		
+//		Actions actions = new Actions(driver);
+//		actions.moveToElement(removePlayerLink).click().perform();
+//		
+//		removePlayerLink.click();
+		
+		System.out.println("3333333333333");
 
 		// Find submitButton
-		WebElement submitButton = driver.findElement(BY_SUBMIT_TRADE_BUTTON);
-		submitButton.click();
+		WebElement continueButton = driver.findElement(BY_CONTINUE_TRADE_BUTTON);
+		continueButton.click();
+		
+		System.out.println("44444444444444");
+		
+		// Find submitButton
+//		WebElement submitButton = driver.findElement(BY_SUBMIT_TRADE_BUTTON);
+//		submitButton.click();
 
 		// Find confirmButton
-		WebElement confirmButton = driver.findElement(BY_CONFIRM_TRADE_BUTTON);
+		WebElement confirmButton = wait.until(ExpectedConditions.elementToBeClickable(BY_CONFIRM_TRADE_BUTTON));
 		confirmButton.click();
 
 		System.out.println("Test Ended! " + Thread.currentThread().getId());
