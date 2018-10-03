586077955c9dee6356d7b5ded30ba16cc9178919
raul <rpberenguer@gmail.com>
2018-10-03 23:33:21 +0200

    fixes

--- src/main/java/es/fantasymanager/services/TradeParserServiceImpl.java
+++ src/main/java/es/fantasymanager/services/TradeParserServiceImpl.java
@@ -27,54 +27,56 @@ public class TradeParserServiceImpl implements TradeParserService, Constants {
 
 		log.info("Trade Started! " + Thread.currentThread().getId());
 
-		hub.setupDriver("chrome");
-
+		
 		// Get driver
+		hub.setupDriver("chrome");
 		WebDriver driver = hub.getDriver();
+		
+		try {
+			driver.get(URL_ADD_PLAYERS);
+			driver.switchTo().defaultContent(); // you are now outside both frames
+			driver.switchTo().frame(LOGIN_IFRAME);
+			driver.getPageSource();
 
-		driver.get(URL_ADD_PLAYERS);
-		driver.switchTo().defaultContent(); // you are now outside both frames
-		driver.switchTo().frame(LOGIN_IFRAME);
-		driver.getPageSource();
+			// Print the title
+			log.info("Title: " + driver.getTitle());
 
-		// Print the title
-		log.info("Title: " + driver.getTitle());
+			// Login
+			final WebElement email = driver.findElement(BY_EMAIL_INPUT);
+			email.sendKeys("rpberenguer@gmail.com");
 
-		// Login
-		final WebElement email = driver.findElement(BY_EMAIL_INPUT);
-		email.sendKeys("rpberenguer@gmail.com");
+			final WebElement password = driver.findElement(BY_PASSWORD_INPUT);
+			password.sendKeys("8ad3aah4");
 
-		final WebElement password = driver.findElement(BY_PASSWORD_INPUT);
-		password.sendKeys("8ad3aah4");
+			final WebElement signupButton = driver.findElement(BY_SUBMIT_LOGIN_BUTTON);
+			signupButton.click();
 
-		final WebElement signupButton = driver.findElement(BY_SUBMIT_LOGIN_BUTTON);
-		signupButton.click();
+			// Wait WebDriver
+			driver.switchTo().defaultContent();
+			WebDriverWait wait = new WebDriverWait(driver, 90);
 
-		// Wait WebDriver
-		driver.switchTo().defaultContent();
-		WebDriverWait wait = new WebDriverWait(driver, 90);
+			// Find (+) Add Player Linkg & click()
+			WebElement addPlayerLink = wait.until(
+					ExpectedConditions.elementToBeClickable(By.xpath(String.format(ADD_PLAYER_LINK, playerToAdd))));
+			addPlayerLink.click();
 
-		// Find (+) Add Player Linkg & click()
-		WebElement addPlayerLink = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format(ADD_PLAYER_LINK, playerToAdd))));
-		addPlayerLink.click();
+			// Find checkBox to Remove Player
+			WebElement removePlayerLink = wait.until(ExpectedConditions
+					.elementToBeClickable(By.xpath(String.format(REMOVE_PLAYER_LINK, playerToRemove))));
+			// js executor
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();", removePlayerLink);
 
-		// Find checkBox to Remove Player
-		WebElement removePlayerLink = wait.until(
-				ExpectedConditions.elementToBeClickable(By.xpath(String.format(REMOVE_PLAYER_LINK, playerToRemove))));
-		// js executor
-		((JavascriptExecutor) driver).executeScript("arguments[0].click();", removePlayerLink);
+			// Find submitButton
+			WebElement continueButton = driver.findElement(BY_CONTINUE_TRADE_BUTTON);
+			continueButton.click();
 
-		// Find submitButton
-		WebElement continueButton = driver.findElement(BY_CONTINUE_TRADE_BUTTON);
-		continueButton.click();
-
-		// Find confirmButton
-		WebElement confirmButton = wait.until(ExpectedConditions.elementToBeClickable(BY_CONFIRM_TRADE_BUTTON));
-		confirmButton.click();
-
-		// Quit driver
-		driver.quit();
+			// Find confirmButton
+			WebElement confirmButton = wait.until(ExpectedConditions.elementToBeClickable(BY_CONFIRM_TRADE_BUTTON));
+			confirmButton.click();
+		} finally {
+			// Quit driver
+			driver.quit();
+		}
 
 		log.info("Trade Ended! " + Thread.currentThread().getId());
 	}
